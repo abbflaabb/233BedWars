84edd70df1b504675534f3cc216cb0140b467cde
SerendipityR <48401197+SerendipityR-2022@users.noreply.github.com>
2025-07-03 13:30:01 +0800

    Fix: Title为空时发送SubTitle不生效

--- src/cn/serendipityr/_233bedwars/utils/TitleUtil.java
+++ src/cn/serendipityr/_233bedwars/utils/TitleUtil.java
@@ -4,6 +4,12 @@ import org.bukkit.entity.Player;
 
 public class TitleUtil {
     public static void send(Player p, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+        if (title.trim().isEmpty()) {
+            if (subtitle.trim().isEmpty()) {
+                return;
+            }
+            title = "§r";
+        }
         ProviderUtil.bw.getVersionSupport().sendTitle(p, title, subtitle, fadeIn, stay, fadeOut);
     }
 }
